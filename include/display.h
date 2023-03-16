7660070199b94f492b2d35e4af0e76f0c24fed93
kvIT-389 <64485615+kvIT-389@users.noreply.github.com>
2023-03-17 04:14:50 +0800

    Add "display" header and code files with draw and display functions

---
+++ include/display.h
@@ -0,0 +1,15 @@
+#ifndef DISPLAY_H_INCLUDED_
+#define DISPLAY_H_INCLUDED_
+
+
+extern double angle;
+
+
+/* Main display function */
+void display(void);
+
+/* Draws RGB hexagon rotated on angle */
+void drawHexagon(void);
+
+
+#endif  /* DISPLAY_H_INCLUDED_ */
--- src/main.c
+++ src/main.c
@@ -1,41 +1,11 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 
+#include "display.h"
+
 
 const unsigned int update_delay = 10;
 
-
-double angle = 0.0;
-
-void drawHexagon()
-{
-    glPushMatrix();
-
-    glRotated(angle, 0.0, 0.0, 1.0);
-
-    glBegin(GL_POLYGON);
-        glColor3ub(0xff, 0x00, 0x00);  glVertex2d( 1.0,  0.0);
-        glColor3ub(0x7f, 0x7f, 0x00);  glVertex2d( 0.5,  0.87);
-        glColor3ub(0x00, 0xff, 0x00);  glVertex2d(-0.5,  0.87);
-        glColor3ub(0x00, 0x7f, 0x7f);  glVertex2d(-1.0,  0.0);
-        glColor3ub(0x00, 0x00, 0xff);  glVertex2d(-0.5, -0.87);
-        glColor3ub(0x7f, 0x00, 0x7f);  glVertex2d( 0.5, -0.87);
-    glEnd();
-
-    glPopMatrix();
-}
-
-void display()
-{
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glLoadIdentity();
-
-    drawHexagon();
-
-    glutSwapBuffers();
-}
 
 void update(int value)
 {
