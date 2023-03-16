bd275717ea33fa3120135f4b1ca15489aa390a53
kvIT-389 <64485615+kvIT-389@users.noreply.github.com>
2023-03-17 02:37:33 +0800

    Add update function to redraw frames and rotate hexagon

--- src/main.c
+++ src/main.c
@@ -2,8 +2,17 @@
 #include <GL/glut.h>
 
 
+const unsigned int update_delay = 10;
+
+
+float angle = 0.0;
+
 void drawHexagon()
 {
+    glPushMatrix();
+
+    glRotatef(angle, 0.0, 0.0, 1.0);
+
     glBegin(GL_POLYGON);
         glColor3ub(0xff, 0x00, 0x00);  glVertex2f( 1.0f,  0.0f);
         glColor3ub(0x7f, 0x7f, 0x00);  glVertex2f( 0.5f,  0.87f);
@@ -12,6 +21,8 @@ void drawHexagon()
         glColor3ub(0x00, 0x00, 0xff);  glVertex2f(-0.5f, -0.87f);
         glColor3ub(0x7f, 0x00, 0x7f);  glVertex2f( 0.5f, -0.87f);
     glEnd();
+
+    glPopMatrix();
 }
 
 void display()
@@ -26,6 +37,13 @@ void display()
     glutSwapBuffers();
 }
 
+void update(int value)
+{
+    angle = (int)(angle + 1.0f) % 360;
+
+    glutPostRedisplay();
+    glutTimerFunc(update_delay, update, value);
+}
 
 int main(int argc, char **argv)
 {
@@ -35,9 +53,10 @@ int main(int argc, char **argv)
     glutInitWindowSize(480, 480);
     glutInitWindowPosition(200, 200);
 
-    glutCreateWindow("RGB hexagon");
+    glutCreateWindow("Rotating hexagon");
 
     glutDisplayFunc(display);
+    glutTimerFunc(update_delay, update, 0);
 
     glutMainLoop();
 
