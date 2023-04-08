3d9f77f9c88842d312445f7990ed9d09f8bdbc0f
kvIT-389 <64485615+kvIT-389@users.noreply.github.com>
2023-04-09 01:22:25 +0800

    Add Image structure to store loaded image data

--- src/texture.c
+++ src/texture.c
@@ -1,9 +1,7 @@
 #include <GL/gl.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb/stb_image.h"
-
 #include "texture.h"
+#include "image.h"
 
 
 Texture test_texture;
@@ -13,29 +11,35 @@ void textureLoad(Texture *texture, const char *file_name)
 {
     if (texture->id) return;  /* Error: texture is already loaded. */
 
-    Size size;
-    int channels_count;
+    Image image = {};
+    imageLoad(&image, file_name);
 
-    _rgba_channel_t *data = stbi_load(
-        file_name, &size.width, &size.height, &channels_count, 0
-    );
+    texture->size = image.size;
 
-    texture->size = size;
-
-    GLint color_format = (channels_count == 4) ? GL_RGBA : GL_RGB;
+    GLint color_format;
+    if (image.channels_count == 4) {
+        color_format = GL_RGBA;
+    }
+    else {
+        color_format = GL_RGB;
+    }
 
     glGenTextures(1, &texture->id);
+
     glBindTexture(GL_TEXTURE_2D, texture->id);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        glTexImage2D(GL_TEXTURE_2D, 0, color_format, size.width, size.height,
-                                    0, color_format, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(
+            GL_TEXTURE_2D, 0, color_format,
+            image.size.width, image.size.height,
+            0, color_format, GL_UNSIGNED_BYTE, image.data
+        );
     glBindTexture(GL_TEXTURE_2D, 0);
 
-    stbi_image_free(data);
+    imageFree(&image);
 }
 
 void textureFree(Texture *texture)
@@ -44,5 +48,5 @@ void textureFree(Texture *texture)
 
     glDeleteTextures(1, &texture->id);
 
-    *texture = (Texture){0, 0, 0};
+    *texture = (Texture){};
 }
